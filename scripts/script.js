c3e681baf27272fc14b1bb4e86a2ba982a1ae7b4
djzee5 <113630697+djzee5@users.noreply.github.com>
2022-11-19 13:17:17 -0500

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+/*
+    Student Name: Samuel Ayoola
+    File Name: script.js
+    Date: 11/19/2022
+*/
+
+/**
+ * jQuery function to hide paragraph elements within
+ * the article elements.
+ */
+function remove() {
+    // Hide all paragraph elements within article
+    // elements using hide() function of JQuery
+    $("article > p").hide();
+}
+
+/**
+ * jQuery function to show paragraph elements within
+ * the article elements.
+ */
+function display() {
+    // Show all paragraph elements within article
+    // elements using show() function of JQuery
+    $("article > p").show();
+}
+
+
